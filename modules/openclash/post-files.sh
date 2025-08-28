8706405817b3d5f88e081928f4691d9f8d8010c1
hfy2025 <hongfy2025@gmail.com>
2025-08-28 10:15:38 +0800

    Create post-files.sh

---
+++ modules/openclash/post-files.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# 下载 OpenClash 核心
+wget -O /etc/openclash/core/clash https://github.com/vernesong/OpenClash/releases/download/Clash/clash-linux-arm64.tar.gz
+tar -xzf /etc/openclash/core/clash-linux-arm64.tar.gz -C /etc/openclash/core/
